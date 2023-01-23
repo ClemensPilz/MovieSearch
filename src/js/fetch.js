7068e7aecd28647a4ca06d2ebe281602e3c55946
Clemens Pilz <office@clemenspilz.com>
2023-01-23 01:26:43 +0100

    Fixed duplicate movie results and cards /wo imgs

--- src/js/fetch.js
+++ src/js/fetch.js
@@ -1,4 +1,4 @@
-// Task: Add error handling
+// Task: Add error handling.
 
 
 //Generates a random query-url from a genre-id, then calls addRandom to select random movies from that result and add them as cards to a target html-element.
@@ -14,15 +14,16 @@ export async function getRandom(id, target, count) {
   //Concatenate URL for query
   let url = 'https://api.themoviedb.org/3/discover/movie?api_key=842edf5aa8d511e033aa2536e59e3fb4&language=en-US&sort_by=' + sort[sortNum] + '&include_adult=false&with_genres=' + id + '&page=' + pageNumber;
 
-  let x = await addRandom(url, target, count);
-  while (x > 0) {
-    x = await addRandom(url, target, x)
+  let obj = await addRandom(url, target, count, []);
+  console.log(obj);
+  while (obj.count > 0) {
+    obj = await addRandom(url, target, obj.count, obj.done)
   }
 
 
 }
 
-async function addRandom(url, target, count) {
+async function addRandom(url, target, count, done) {
   return new Promise((resolve) => {
     fetch(url)
       .then(res => res.json())
@@ -31,18 +32,26 @@ async function addRandom(url, target, count) {
         //Select random movie from page
         let i = Math.floor(Math.random() * res.results.length);
 
-        //Append movie details to target
-        let title = res.results[i].original_title;
-        let release = res.results[i].release_date;
-        let imageURL = "https://image.tmdb.org/t/p/original" + res.results[i].poster_path;
-        let theDiv = document.createElement('div');
-        theDiv.classList.add('card');
-        theDiv.innerHTML = title + ", " + release + " <br>"
-          + '<img src="' + imageURL + '">';
-        target.appendChild(theDiv);
-        return res;
-      })
 
-      .then(resolve(--count))
+        if (!done.includes(i)) {
+          done.push(i);
+          //Append movie details to target
+          let title = res.results[i].original_title;
+          let release = res.results[i].release_date;
+          let imageURL = "https://image.tmdb.org/t/p/original" + res.results[i].poster_path;
+          let theDiv = document.createElement('div');
+          theDiv.classList.add('card');
+          theDiv.innerHTML = title + ", " + release + " <br>"
+            + '<img src="' + imageURL + '">';
+          if (!theDiv.innerHTML.includes("null")) {
+            target.appendChild(theDiv);
+            count--;
+          }
+        }
+
+        return { count: count, done: done };
+
+      })
+      .then((res) => resolve(res));
   })
 }
